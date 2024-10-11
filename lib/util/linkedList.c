db45b7bb4262ed5a6db781f00ec6c44ce826a8db
Universumgames <programming@universegame.de>
2024-10-11 19:57:08 +0200

    add linked list

---
+++ lib/util/linkedList.c
@@ -0,0 +1,105 @@
+#include "linkedList.h"
+
+struct __list {
+    node_t* head;
+    node_t* tail;
+    size_t size;
+};
+
+typedef struct {
+    void* data;
+    node_t* next;
+} node_t;
+
+list_t* list_create() {
+    list_t* list = malloc(sizeof(list_t));
+    list->head = NULL;
+    list->tail = NULL;
+    list->size = 0;
+    return list;
+}
+
+void list_destroy(list_t* list) {
+    node_t* current = list->head;
+    while (current != NULL) {
+        node_t* next = current->next;
+        free(current);
+        current = next;
+    }
+    free(list);
+}
+
+void list_push_back(list_t* list, void* data) {
+    node_t* node = malloc(sizeof(node_t));
+    node->data = data;
+    node->next = NULL;
+    if (list->tail == NULL) {
+        list->head = node;
+        list->tail = node;
+    } else {
+        list->tail->next = node;
+        list->tail = node;
+    }
+    list->size++;
+}
+
+void list_push_front(list_t* list, void* data) {
+    node_t* node = malloc(sizeof(node_t));
+    node->data = data;
+    node->next = list->head;
+    list->head = node;
+    if (list->tail == NULL) {
+        list->tail = node;
+    }
+    list->size++;
+}
+
+void* list_pop_back(list_t* list) {
+    if (list->size == 0) {
+        return NULL;
+    }
+    void* data = list->tail->data;
+    node_t* current = list->head;
+    while (current->next != list->tail) {
+        current = current->next;
+    }
+    free(list->tail);
+    list->tail = current;
+    list->size--;
+    return data;
+}
+
+void* list_pop_front(list_t* list) {
+    if (list->size == 0) {
+        return NULL;
+    }
+    void* data = list->head->data;
+    node_t* next = list->head->next;
+    free(list->head);
+    list->head = next;
+    list->size--;
+    return data;
+}
+
+void* list_get(list_t* list, size_t index) {
+    if (index >= list->size) {
+        return NULL;
+    }
+    node_t* current = list->head;
+    for (size_t i = 0; i < index; i++) {
+        current = current->next;
+    }
+    return current->data;
+}
+
+size_t list_size(list_t* list) {
+    return list->size;
+}
+
+void list_foreach(list_t* list, void (*callback)(void*)) {
+    node_t* current = list->head;
+    while (current != NULL) {
+        callback(current->data);
+        current = current->next;
+    }
+}
---
+++ lib/util/linkedList.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <stddef.h>
+
+struct __list;
+
+typedef struct __list list_t;
+
+list_t* list_create();
+
+void list_destroy(list_t* list);
+
+void list_push_back(list_t* list, void* data);
+
+void list_push_front(list_t* list, void* data);
+
+void* list_pop_back(list_t* list);
+
+void* list_pop_front(list_t* list);
+
+void* list_get(list_t* list, size_t index);
+
+size_t list_size(list_t* list);
+
+void list_foreach(list_t* list, void (*callback)(void*));
