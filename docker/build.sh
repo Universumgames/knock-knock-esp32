2012cc51c8e88ab6b398a7fe18599f15be018a41
Universumgames <programming@universegame.de>
2024-10-17 20:35:42 +0200

    try fixing ci/cd

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+# Login to the Docker registry
+# docker login registry.ide3.de
 
 # Build the Docker image
-docker build -t universumgames/pio_esp32:0.1 .
+docker build -t registry.ide3.de/universumgames/mic-projekt .
+
+# Push the Docker image
+docker push registry.ide3.de/universumgames/mic-projekt
