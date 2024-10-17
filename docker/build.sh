457fd4af31335f8a9289a30ea0533181893ca748
Universumgames <programming@universegame.de>
2024-10-18 01:48:14 +0200

    fix cicd try 7

--- docker/build.sh
+++ docker/build.sh
@@ -3,7 +3,7 @@
 # docker login registry.ide3.de
 
 # Build the Docker image
-docker buildx build --platform linux/amd64 -t registry.ide3.de/universumgames/mic-projekt/pio_build_esp32 .
+docker buildx build --platform linux/amd64 --provenance=false -t registry.ide3.de/universumgames/mic-projekt/pio_build_esp32 .
 
 # Push the Docker image
 docker push registry.ide3.de/universumgames/mic-projekt/pio_build_esp32
