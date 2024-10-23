004213ba00672219b7751ead2c29c8951be74566
Universumgames <programming@universegame.de>
2024-10-23 23:44:49 +0200

    trying out i2s (not working at all)

--- lib/led_strip/FastLED.c
+++ lib/led_strip/FastLED.c
@@ -1,6 +1,7 @@
 #include "FastLed.h"
 
 #include <driver/gpio.h>
+#include <driver/i2s_std.h>
 #include <driver/spi_master.h>
 #include <esp_log.h>
 #include <freertos/FreeRTOS.h>
@@ -9,45 +10,24 @@
 
 const char* TAG_FAST_LED = "FastLED";
 
-#define WS2812_SPI_SPEED_HZ (2400 * 1000)
-#define WS2812_LED_BUF (9)
-#define WS2812_LED_BUF_BIT (WS2812_LED_BUF * 8)
+#define WS1812_ENCODED_BYTES_PER_BYTE (4)
+#define WS2812_BYTES_PER_LED (3 * WS1812_ENCODED_BYTES_PER_BYTE)
+#define SAMPLE_RATE (93750)
+#define ZERO_BUFFER 48
 
-/**
- * @brief write data to spi
- *
- * @param spi_handle the spi handle
- * @param buf the buffer to write
- * @param size the size of the buffer
- * @return esp_err_t error code
- */
-esp_err_t _write(spi_device_handle_t spi_handle, uint8_t* buf, size_t size) {
-    spi_transaction_t t = {0};
-
-    t.length = size;
-    t.tx_buffer = buf;
-    t.rx_buffer = NULL;
-
-    return spi_device_transmit(spi_handle, &t);
-}
+static const uint16_t bitpatterns[4] = {0x88, 0x8e, 0xe8, 0xee};
 
 /**
  * @brief Clever way to encode the WS2812 protocol into SPI bits
- * (for example calculation see documentation/assets/ws2812_spi_bit_encode.jpeg)
  *
  * @param data the byte to encode
  * @param buf the buffer to write the encoded data to
  */
 void set_data_bit(uint8_t data, uint8_t* buf) {
-    *(buf + 2) |= data & BIT(0) ? BIT(2) | BIT(1) : BIT(2);
-    *(buf + 2) |= data & BIT(1) ? BIT(5) | BIT(4) : BIT(5);
-    *(buf + 2) |= data & BIT(2) ? BIT(7) : 0x00;
-    *(buf + 1) |= data & BIT(2) ? BIT(0) : BIT(0);
-    *(buf + 1) |= data & BIT(3) ? BIT(3) | BIT(2) : BIT(3);
-    *(buf + 1) |= data & BIT(4) ? BIT(6) | BIT(5) : BIT(6);
-    *(buf + 0) |= data & BIT(5) ? BIT(1) | BIT(0) : BIT(1);
-    *(buf + 0) |= data & BIT(6) ? BIT(4) | BIT(3) : BIT(4);
-    *(buf + 0) |= data & BIT(7) ? BIT(7) | BIT(6) : BIT(7);
+    *(buf + 0) = bitpatterns[(data >> 6) & 0x03];
+    *(buf + 1) = bitpatterns[(data >> 4) & 0x03];
+    *(buf + 2) = bitpatterns[(data >> 2) & 0x03];
+    *(buf + 3) = bitpatterns[(data >> 0) & 0x03];
 }
 
 /**
@@ -60,68 +40,85 @@ void set_data_bit(uint8_t data, uint8_t* buf) {
  * @param buf the buffer to write the data to
  */
 void generate_data(uint32_t index, uint8_t red, uint8_t green, uint8_t blue, uint8_t* buf) {
-    memset(buf + (index)*WS2812_LED_BUF, 0, WS2812_LED_BUF);
-    set_data_bit(green, buf + (index)*WS2812_LED_BUF);
-    set_data_bit(red, buf + (index)*WS2812_LED_BUF + 3);
-    set_data_bit(blue, buf + (index)*WS2812_LED_BUF + 6);
+    uint8_t* startAddr = buf + index * WS2812_BYTES_PER_LED;
+    memset(startAddr, 0, WS2812_BYTES_PER_LED);
+    set_data_bit(green, startAddr);
+    set_data_bit(red, startAddr + WS1812_ENCODED_BYTES_PER_BYTE);
+    set_data_bit(blue, startAddr + WS1812_ENCODED_BYTES_PER_BYTE * 2);
 }
+
+typedef struct {
+    uint8_t red;
+    uint8_t green;
+    uint8_t blue;
+} RGB_LED;
 
 struct __FastLED_Config_internal {
     gpio_num_t pin_number;
-    spi_device_handle_t spi_handle;
-    uint8_t* led_strip_pixels;
-    int lengthRGB;
-    int lengthPixels;
+    RGB_LED* led_data;
+    int lengthRBGStrip;
+    uint8_t* encoded_led_data;
+    int lengthEncoded;
+
+    i2s_chan_handle_t tx_handle;
+    i2s_chan_config_t chan_cfg;
+    i2s_std_config_t std_cfg;
 };
 
 FastLEDConfig* initFastLED(int length, gpio_num_t pin_number) {
     FastLEDConfig* config = (FastLEDConfig*)calloc(sizeof(FastLEDConfig), 1);
     *config = (FastLEDConfig){
         .pin_number = pin_number,
-        .led_strip_pixels = heap_caps_calloc(length, WS2812_LED_BUF, MALLOC_CAP_8BIT),
-        .lengthRGB = length,
-        .lengthPixels = length * WS2812_LED_BUF,
+        .led_data = (RGB_LED*)calloc(sizeof(RGB_LED), length),
+        .lengthRBGStrip = length,
+        .encoded_led_data = (uint8_t*)calloc(sizeof(uint8_t), length * WS2812_BYTES_PER_LED),
+        .lengthEncoded = length * WS2812_BYTES_PER_LED,
+
     };
 
-    gpio_set_drive_capability(config->pin_number, GPIO_DRIVE_CAP_3);
-
-    spi_bus_config_t bus_config = {
-        .mosi_io_num = config->pin_number,
-        .miso_io_num = -1,
-        .sclk_io_num = -1,
-        .quadwp_io_num = -1,
-        .quadhd_io_num = -1,
-        .max_transfer_sz = config->lengthRGB * WS2812_LED_BUF_BIT,
+    config->chan_cfg = (i2s_chan_config_t)I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_AUTO, I2S_ROLE_MASTER);
+    i2s_new_channel(&config->chan_cfg, &config->tx_handle, NULL);
+    config->std_cfg = (i2s_std_config_t){
+        .clk_cfg = I2S_STD_CLK_DEFAULT_CONFIG(93750),
+        .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_32BIT, I2S_SLOT_MODE_MONO),
+        .gpio_cfg = {
+            .mclk = I2S_GPIO_UNUSED,
+            .bclk = I2S_GPIO_UNUSED,
+            .ws = I2S_GPIO_UNUSED,
+            .dout = pin_number,
+            .din = I2S_GPIO_UNUSED,
+            .invert_flags = {
+                .mclk_inv = false,
+                .bclk_inv = false,
+                .ws_inv = false,
+            },
+        },
     };
 
-    spi_device_interface_config_t dev_config = {
-        .command_bits = 0,
-        .address_bits = 0,
-        .dummy_bits = 0,
-        .clock_speed_hz = WS2812_SPI_SPEED_HZ,
-        .duty_cycle_pos = 128,
-        .mode = 0,
-        .spics_io_num = -1,
-        .queue_size = 1,
-    };
+    i2s_channel_init_std_mode(config->tx_handle, &config->std_cfg);
+    i2s_channel_enable(config->tx_handle);
 
-    spi_bus_initialize(SPI2_HOST, &bus_config, SPI_DMA_CH_AUTO);
-
-    spi_bus_add_device(SPI2_HOST, &dev_config, &config->spi_handle);
-
-    _write(config->spi_handle, config->led_strip_pixels, config->lengthPixels);
+    showLED(config);
+    writeLED(config, 0, 128, 128, 128);
+    showLED(config);
 
     return config;
 }
 
 void writeLED(FastLEDConfig* config, int index, uint8_t red, uint8_t green, uint8_t blue) {
-    if (index >= config->lengthRGB) {
+    if (index >= config->lengthRBGStrip) {
         ESP_LOGE(TAG_FAST_LED, "Index out of range: %d", index);
         return;
     }
-    generate_data(index, red, green, blue, config->led_strip_pixels);
+    config->led_data[index] = (RGB_LED){
+        .red = red,
+        .green = green,
+        .blue = blue,
+    };
+    generate_data(index, red, green, blue, config->encoded_led_data);
 }
 
 void showLED(FastLEDConfig* config) {
-    _write(config->spi_handle, config->led_strip_pixels, config->lengthRGB * WS2812_LED_BUF_BIT);
+    size_t bytes_written = 0;
+    i2s_channel_write(config->tx_handle, config->encoded_led_data, config->lengthEncoded, NULL, pdMS_TO_TICKS(100));
 }
