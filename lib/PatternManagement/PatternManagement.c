23721ce4ee7622c611f49f9b62abaf6cd20e7b7b
joarn002 <jovan.arnaut@stud.hn.de>
2024-12-05 20:39:59 +0100

    PatternManagmentdraft

--- lib/PatternManagement/PatternManagement.c
+++ lib/PatternManagement/PatternManagement.c
@@ -2,26 +2,80 @@
 #include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
+#include "lock_status.h"
+#include "PatternStorage.h"
+#include "pattern_play.h"
 
 static void IRAM_ATTR gpio_isr_handler(void* arg);
+static void patternManagerThread(void* arg);
 
+QueueHandle_t queue;
+
+/**
+ * Configure button for input pin and configure interrupt
+ */
 void setupButton(gpio_num_t pin) {
     gpio_reset_pin(pin);
     gpio_set_direction(pin, GPIO_MODE_INPUT);
     gpio_set_pull_mode(pin, GPIO_PULLDOWN_ONLY);
     gpio_set_intr_type(pin, GPIO_INTR_POSEDGE);
-    gpio_install_isr_service(0);
     gpio_isr_handler_add(pin, gpio_isr_handler, (void*)&pin);
     gpio_intr_enable(pin);
 }
 
 void initPatternManagement() {
+    queue = xCreateQueue(8, sizeof(gpio_num_t));
+    xTaskCreate(patternManagerThread, "pattern_management_thread", THREAD_STACK_MANAGEMENT,
+                NULL, THREAD_PRIO_MANAGEMENT, NULL);
+
+    gpio_install_isr_service(0);
     setupButton((gpio_num_t)PIN_BUTTON_PLAY);
-    //...
+    setupButton((gpio_num_t)PIN_BUTTON_UP);
+    setupButton((gpio_num_t)PIN_BUTTON_DOWN);
+    setupButton((gpio_num_t)PIN_BUTTON_RECORD);
+    setupButton((gpio_num_t)PIN_BUTTON_DELETE);
 }
 
+/**
+ * fast interrupt handler
+ */
 static void IRAM_ATTR gpio_isr_handler(void* arg) {
     gpio_num_t pin = *(gpio_num_t*)arg;
-    // Do something, keep it short, work with queues and a separate while(true)
-    // task if needed or create a new task here
+    // send pin "press" (gpio_num_t) to queue
+    xQueueSend(queue, (void*) pin, (TickType_t)0);
+}
+
+static size_t selectedPatternIndex = 0;
+
+static void patternManagerThread(void* arg){
+    gpio_num_t pin = 0;
+    while(true){
+        // check if queue contains item (stored automatically in pin), if not wait
+        if(!xQueueReceive(queue, &pin, (TickType_t)5))
+            vTaskDelay(pdMS_TO_TICKS(50));
+
+        LinkedList patterns = getPatterns();
+        size_t patternLength = list_size(patterns);
+
+        if(pin == PIN_BUTTON_PLAY){
+            // play button was pressed
+            PatternData* toPlay = list_get(patterns, selectedPatternIndex);
+            playPattern(toPlay);
+        }else if (pin == PIN_BUTTON_UP){
+            // up button was pressed
+            selectedPatternIndex = (selectedPatternIndex + patternLength - 1) % patternLength;
+        }else if (pin == PIN_BUTTON_DOWN){
+           // down button was pressed
+            selectedPatternIndex = (selectedPatternIndex + 1) % patternLength;
+        }else if (pin == PIN_BUTTON_RECORD){
+            // record button was pressed
+            updateLEDStatus(MUSTER_AUFNAHME);
+        }else if (pin == PIN_BUTTON_DELETE){
+            // delete button was pressed
+            PatternData* toDelete = list_get(patterns, selectedPatternIndex);
+            deletePattern(toDelete->id);
+        }
+
+    }
 }
